051150321bccd67aa8144aa48eb37c544786d3ba
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-15 15:34:36 +0100

    Read file

--- file.js
+++ file.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 // fs -> file system
 
-fs.writeFileSync('hello.txt', 'Hello from Node.js');
+// fs.writeFileSync('hello.txt', 'Hello from Node.js');
+
+
+// fs.readFile(path[, options], callback)
+//                 en-coding format | get the result
+
+fs.readFile('hello.txt', 'utf8', function(err, dataStr){
+    console.log(err);
+    console.log('----------');
+    console.log(dataStr);
+});
