78d2742a8a9ac7f9041214a1660ec37adfe9ee3b
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-18 15:23:49 +0100

    Understand arrow function

---
+++ arrowFunc.js
@@ -0,0 +1,12 @@
+const name = 'Victor';
+let age = 20;
+const hasHobbies = true;
+
+age = 25;
+
+const summerizeUser = function (userName, userAge, userHasHobby) {
+    return ('Name is ' + userName + ', age is ' + userAge +
+            ' and the user has hobbies: ' + userHasHobby);
+}
+
+console.log(summerizeUser(name, age, hasHobbies));
