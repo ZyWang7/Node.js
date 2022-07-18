28b48d52d8bc1fadfee133d0f52f93ddc8658889
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-18 15:48:34 +0100

    Add more functions

--- arrowFunc.js
+++ arrowFunc.js
@@ -13,4 +13,12 @@ const summerizeUser = (userName, userAge, userHasHobby) => {
             ' and the user has hobbies: ' + userHasHobby);
 };
 
+const add = (a, b) => a + b;
+const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
 console.log(summerizeUser(name, age, hasHobbies));
+
+console.log(add(1, 2));
+console.log(addOne(1));
+console.log(addRandom());
