55350f06555576c57ffea54762687a7618b9a651
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-18 15:34:55 +0100

    Create arrow function

--- arrowFunc.js
+++ arrowFunc.js
@@ -4,9 +4,13 @@ const hasHobbies = true;
 
 age = 25;
 
-const summerizeUser = function (userName, userAge, userHasHobby) {
+// const summerizeUser = function (userName, userAge, userHasHobby) {
+//     return ('Name is ' + userName + ', age is ' + userAge +
+//             ' and the user has hobbies: ' + userHasHobby);
+// }
+const summerizeUser = (userName, userAge, userHasHobby) => {
     return ('Name is ' + userName + ', age is ' + userAge +
             ' and the user has hobbies: ' + userHasHobby);
-}
+};
 
 console.log(summerizeUser(name, age, hasHobbies));
