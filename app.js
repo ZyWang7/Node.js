c4ffda96a8385d85e1b7fd56be8373c9b5871427
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-19 16:48:31 +0100

    Add response

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ http.createServer(rqListener);
 
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // peocess.exit();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');    // write data to response
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body>Hello from my Node.js Server!</body>');
+    res.write('</html>');
+    res.end();
+    // res.write();     //-> will cause an error
 });     // return a http.server
 
 // not immediately exit script, but keep running to listen for requests
