c9736174d6d8b44776d2c99d983cacc2e2bdbde0
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-23 16:38:24 +0100

    Add middleware

--- expressJs/app.js
+++ expressJs/app.js
@@ -5,6 +5,19 @@ const express = require('express');
 // create an express application -> valid request handler
 const app = express();
 
+// add a middleware function
+// accepts an array of request handlers
+//                 next: a function that will be passed to 
+// this [(req, res, next) => {} ]function by express.js
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();         // allows the request to go to the next middleware in line
+});      // () => {}: will be executed for every incoming request
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
