7f66eb79099cfe2a9e1c1324c3364d5fb8ad0a75
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-23 16:43:33 +0100

    Send a response

--- expressJs/app.js
+++ expressJs/app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middleware!');
+    res.send('<h1>Hello from express</h1>');    // sending a response
 });
 
 const server = http.createServer(app);
