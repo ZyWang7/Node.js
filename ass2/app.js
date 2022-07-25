9864fc2d766c6a158f896cebef9a009fda9a14a1
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-25 15:38:47 +0100

    Add response

--- ass2/app.js
+++ ass2/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.use('/users', (req, res, next) => {
-
+    console.log('In the user page!');
+    res.send('<h1>Users</h1><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>');    // sending a response
 });
 
 app.use('/', (req, res, next) => {
-
+    console.log('Welcome to My 2nd task.');
 });
 
+app.listen(3000);
