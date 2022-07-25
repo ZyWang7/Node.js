06e5783270bee24738ea0496805b2022c09eff5c
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-25 15:47:46 +0100

    Add feedback on ass2

--- ass2/app.js
+++ ass2/app.js
@@ -2,13 +2,26 @@ const express = require('express');
 
 const app = express();
 
+/* task2.2
+app.use((req, res, next) => {
+    console.log('In the user page!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Welcome to My 2nd task.');
+    res.send('<p>Assignment solved</p>');
+});
+*/
+
 app.use('/users', (req, res, next) => {
     console.log('In the user page!');
-    res.send('<h1>Users</h1><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>');    // sending a response
+    res.send('<h1>Users</h1><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>');
 });
 
 app.use('/', (req, res, next) => {
     console.log('Welcome to My 2nd task.');
+    res.send('<h1>Homepage</h1>');
 });
 
 app.listen(3000);
