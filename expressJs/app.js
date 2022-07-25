be3093bc98895d54443e0be3f01ef1bf390edf40
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-07-25 16:07:57 +0100

    Parse incoming request

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,22 +1,37 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // create an express application -> valid request handler
 const app = express();
-
+/*
 app.use('/', (req, res, next) => {      // every route starts with a '/'
-    console.log('This always runs!');
+    // console.log('This always runs!');
     next();
 });
+*/
 
-app.use('/add-product', (req, res, next) => {      // every route starts with a '/'
-    console.log('In the first middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');    // sending a response
+
+// register some middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/add-product', (req, res, next) => {
+    // console.log('In the first middleware!');
+    // res.send('<h1>The "Add Product" Page</h1>');    // sending a response
+    res.send('<form action="/product" method="post">' +
+             '<input type="text" name="title">' +
+             '<button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);      // { title: 'Milk' }
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
+    // console.log('In another middleware!');
     res.send('<h1>Hello from express</h1>');
 });
 
