e2d738b3e9f7c3413d5fcfa91dff91b1a5f0b35f
Yizheng Sun <yizheng.sun.2000@gmail.com>
2022-07-31 23:25:28 +0100

    Update server.js
    嘻嘻嘻

--- basic/server.js
+++ basic/server.js
@@ -4,7 +4,7 @@ var http = require("http");
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    response.end('<p style="font-size:50pt">Hello world! Wzy 大der🤡<p>');
+    response.end('Hello world! Wzy 大der');
 }).listen(8888);
 
 // print at terminal
