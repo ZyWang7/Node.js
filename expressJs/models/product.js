33725795f5dd472d2d6e2f45cc11c9858a7766cf
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-08-15 11:41:50 +0100

    Store data from files

--- expressJs/models/product.js
+++ expressJs/models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+// const products = [];
+
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(t) {
@@ -7,12 +10,37 @@ module.exports = class Product {
 
     // store the product in the array
     save() {
-        // this -> refer to the object created based on the class
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename),
+                            'data',                 // folder name
+                            'products.json');       // file name
+        
+        // get the existing product -- read the file at path p
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                // convert json to JS array/object
+                products = JSON.parse(fileContent);
+            }
+            
+            // this -> refer to the object created based on the class
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
     // retrieve all the products
     static fetchAll() {
-        return products;
+        const p = path.join(path.dirname(process.mainModule.filename),
+                            'data',
+                            'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return [];
+            }
+
+            return JSON.parse(fileContent);
+        });
     }
 }
