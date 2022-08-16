82fcc216cd62c933ef1d1681db39655973254291
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-08-16 16:30:08 +0100

    Refactor the code

--- expressJs/models/product.js
+++ expressJs/models/product.js
@@ -3,6 +3,24 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename),
+                        'data',
+                        'products.json');
+
+// create a helper function
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            // return [];
+            cb([]);
+        } else {
+            // return JSON.parse(fileContent);
+            cb(JSON.parse(fileContent));
+            // the above return statement -> belongs to the inner function
+        }
+    });
+} 
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
@@ -10,10 +28,15 @@ module.exports = class Product {
 
     // store the product in the array
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename),
-                            'data',                 // folder name
-                            'products.json');       // file name
-        
+        getProductsFromFile(products => {
+            // this -> refer to the object created based on the class
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+
+        /*
         // get the existing product -- read the file at path p
         fs.readFile(p, (err, fileContent) => {
             let products = [];
@@ -21,30 +44,14 @@ module.exports = class Product {
                 // convert json to JS array/object
                 products = JSON.parse(fileContent);
             }
-            
-            // this -> refer to the object created based on the class
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
         });
+        */
     }
 
     // retrieve all the products
     static fetchAll(cb) {
     // the thing calling fetchAll can pass a function is then aware of being
     // called which holds the data I want to return
-        const p = path.join(path.dirname(process.mainModule.filename),
-                            'data',
-                            'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                // return [];
-                cb([]);
-            }
-            // return JSON.parse(fileContent);
-            cb(JSON.parse(fileContent));
-            // the above return statement -> belongs to the inner function
-        });
+        getProductsFromFile(cb);
     }
 }
