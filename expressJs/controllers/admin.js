f6876343346207e1d89e77212e713ea01f2935a1
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-08-16 17:48:07 +0100

    Modify the model

--- expressJs/controllers/admin.js
+++ expressJs/controllers/admin.js
@@ -23,7 +23,12 @@ exports.postAddProduct = (req, res, next) => {
     // products.push({ title: req.body.title });
     // console.log(req.body);      // { title: 'Milk' }
 
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 };
