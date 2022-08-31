81766a239540165c6021ceea61e6fc30fb0bfb99
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-08-31 17:22:04 +0100

    Connect to the database using sequelize

--- expressJs/util/database.js
+++ expressJs/util/database.js
@@ -1,4 +1,5 @@
-const mysql = require('mysql2');
+// using MySQL2 -----------------------------------------------------
+// const mysql = require('mysql2');
 
 // set up one connection and 
 // should always close the connection once done with the query
@@ -9,6 +10,8 @@ can run multiple queries simultaneously
 once the query is done, the connection will be handed back into the pool
 and available for a new query and the pool can then be finished whrn app shuts down
 */
+
+/*
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -17,3 +20,17 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+------------------------------------------------------------------ */
+
+
+// connect sequelize to the database --------------------------------
+// -> use MySQL2 behind the scenes
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node', 'root', 'W@qxx0102Hh-', {   
+                                    dialect: 'mysql',
+                                    host: 'localhost'
+                                });
+// -> will set up a connection pool
+
+module.exports = sequelize;
